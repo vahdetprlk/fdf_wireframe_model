6163d9d0fd9771691ab0fbb1e8882e9523c2749f
vahdetprlk <vahdetparlak@gmail.com>
2023-07-13 19:58:04 +0300

    makefiles_updated

--- src/fdf.c
+++ src/fdf.c
@@ -6,30 +6,14 @@
 /*   By: vparlak <vparlak@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 15:18:05 by vparlak           #+#    #+#             */
-/*   Updated: 2023/07/09 21:46:15 by vparlak          ###   ########.fr       */
+/*   Updated: 2023/07/13 14:41:30 by vparlak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	main(void)
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
-	void	*img_ptr;
-	int		i;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 532, 532, "title");
-	img_ptr = mlx_new_image(mlx_ptr, 300, 300);
-	i = 0;
-	while (500 > i)
-	{
-		
-		i++;
-	}
-
-	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0);
-	mlx_loop(mlx_ptr);
+int main() {
+    ft_printf("hello world\n");
+    return 0;
 }
-
