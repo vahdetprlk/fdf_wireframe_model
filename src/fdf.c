e24e230cd6366132be82e44fefa919311a7099e4
vahdetprlk <vahdetparlak@gmail.com>
2023-08-06 21:45:39 +0300

    put_line_progress

--- src/fdf.c
+++ src/fdf.c
@@ -6,31 +6,107 @@
 /*   By: vparlak <vparlak@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 20:12:30 by vparlak           #+#    #+#             */
-/*   Updated: 2023/08/02 18:16:49 by vparlak          ###   ########.fr       */
+/*   Updated: 2023/08/06 21:17:37 by vparlak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include "mlx.h"
 
-void	put_line(t_mlx_window mlx_window, t_point p_a, t_point p_b, int color)
+void	put_line(t_window window, t_point p_a, t_point p_b, int color)
 {
-	mlx_pixel_put(mlx_window.mlx_ptr, mlx_window.win_ptr, p_a.x, p_a.y, color);
-	mlx_pixel_put(mlx_window.mlx_ptr, mlx_window.win_ptr, p_b.x, p_b.y, color);
+	int	i;
+	int	j;
+	int	k;
+
+	if (p_a.y == p_b.y)
+	{
+		if (p_a.x < p_b.x)
+		{
+			i = p_a.x;
+			while (i <= p_b.x)
+			{
+				mlx_pixel_put(window.mlx, window.win, i, p_a.y, color);
+				i++;
+			}
+		}
+		else if (p_a.x > p_b.x)
+		{
+			i = p_b.x;
+			while (i <= p_a.x)
+			{
+				mlx_pixel_put(window.mlx, window.win, i, p_a.y, color);
+				i++;
+			}
+		}
+	}
+	else if (p_a.x == p_b.x)
+	{
+		if (p_a.y < p_b.y)
+		{
+			i = p_a.y;
+			while (i <= p_b.y)
+			{
+				mlx_pixel_put(window.mlx, window.win, p_a.x, i, color);
+				i++;
+			}
+		}
+		else if (p_a.y > p_b.y)
+		{
+			i = p_b.y;
+			while (i <= p_a.y)
+			{
+				mlx_pixel_put(window.mlx, window.win, p_a.x, i, color);
+				i++;
+			}
+		}
+	}
+	else
+	{
+		j = 0;
+		i = 0;
+		while (i < 100)
+		{
+			mlx_pixel_put(window.mlx, window.win, i, j - 4, 0xAAFFFFFF);
+			mlx_pixel_put(window.mlx, window.win, i, j - 3, 0x88FFFFFF);
+			mlx_pixel_put(window.mlx, window.win, i, j - 2, 0x55FFFFFF);
+			mlx_pixel_put(window.mlx, window.win, i, j - 1, 0x33FFFFFF);
+			k = 0;
+			while (k < 1)
+			{
+				mlx_pixel_put(window.mlx, window.win, i, j, 0x00FFFFFF);
+				j++;
+				k++;
+			}
+			mlx_pixel_put(window.mlx, window.win, i, j, 0x33FFFFFF);
+			mlx_pixel_put(window.mlx, window.win, i, j + 1, 0x55FFFFFF);
+			mlx_pixel_put(window.mlx, window.win, i, j + 2, 0x88FFFFFF);
+			mlx_pixel_put(window.mlx, window.win, i, j + 3, 0xAAFFFFFF);
+			i++;
+		}
+	}
 }
 
 int	main(void)
 {
-	t_mlx_window	mlx_window;
-	t_point			point_a;
-	t_point			point_b;
+	t_window	window;
+	t_point		point_a;
+	t_point		point_b;
+	t_point		point_c;
+	t_point		point_d;
 
-	mlx_window.mlx_ptr = mlx_init();
-	mlx_window.win_ptr = mlx_new_window(mlx_window.mlx_ptr, 500, 500, "Test");
-	point_a.x = 20;
+	window.mlx = mlx_init();
+	window.win = mlx_new_window(window.mlx, 500, 500, "Test");
+	point_a.x = 10;
 	point_a.y = 20;
-	point_b.x = 40;
+	point_b.x = 20;
 	point_b.y = 20;
-	put_line(mlx_window, point_a, point_b, 0x00FFFFFF);
-	mlx_loop(mlx_window.mlx_ptr);
+	point_c.x = 30;
+	point_c.y = 20;
+	point_d.x = 31;
+	point_d.y = 10;
+	put_line(window, point_a, point_b, 0xF0FFFFFF);
+	put_line(window, point_b, point_c, 0xE0FFFFFF);
+	put_line(window, point_d, point_c, 0xA0FFFFFF);
+	mlx_loop(window.mlx);
 }
--- src/fdf.h
+++ src/fdf.h
@@ -6,18 +6,18 @@
 /*   By: vparlak <vparlak@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 16:48:47 by vparlak           #+#    #+#             */
-/*   Updated: 2023/08/02 18:01:42 by vparlak          ###   ########.fr       */
+/*   Updated: 2023/08/06 15:16:16 by vparlak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-typedef struct s_mlx_window
+typedef struct s_window
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-}	t_mlx_window;
+	void	*mlx;
+	void	*win;
+}	t_window;
 
 typedef struct s_point
 {
